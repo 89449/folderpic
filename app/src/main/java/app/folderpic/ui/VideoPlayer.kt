53b4f139000a945b3fd00227bcd7181bb22a4f9d
89449 <riko91209@gmail.com>
2025-09-14 15:56:21 +0700

    Update VideoPlayer.kt

--- app/src/main/java/app/folderpic/ui/VideoPlayer.kt
+++ app/src/main/java/app/folderpic/ui/VideoPlayer.kt
@@ -4,60 +4,37 @@ import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
 @Composable
 fun VideoPlayer(
-    uri: Uri, 
-    currentPage: Boolean, 
+    currentPage: Boolean,
     isPlaying: Boolean,
+    exoPlayer: ExoPlayer,
     modifier: Modifier = Modifier
 ) {
-    val context = LocalContext.current
     val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
 
-    val exoPlayer = remember {
-        ExoPlayer.Builder(context).build().apply {
-            repeatMode = Player.REPEAT_MODE_ONE
-            setMediaItem(MediaItem.fromUri(uri)) 
-            prepare()
-        }
-    }
-
     LaunchedEffect(currentPage, isPlaying) {
-        if (currentPage && isPlaying) {
-            exoPlayer.play()
-        } else {
-            exoPlayer.pause()
-            if (!currentPage) {
-                exoPlayer.seekTo(0)
-            }
+        exoPlayer.playWhenReady = currentPage && isPlaying
+        
+        if (!currentPage) {
+            exoPlayer.seekTo(0)
         }
     }
 
-    DisposableEffect(key1 = lifecycleOwner, key2 = uri) {
+    DisposableEffect(lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_PAUSE -> {
-                    exoPlayer.pause()
-                }
-                Lifecycle.Event.ON_RESUME -> {
-                    if (currentPage && isPlaying) {
-                        exoPlayer.play()
-                    }
-                }
-                else -> {}
+            if (event == Lifecycle.Event.ON_PAUSE) {
+                exoPlayer.pause()
             }
         }
         lifecycleOwner.lifecycle.addObserver(observer)
@@ -67,15 +44,13 @@ fun VideoPlayer(
             lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
-
     AndroidView(
         modifier = modifier,
         factory = {
             PlayerView(it).apply {
-                player = exoPlayer
                 useController = false
+                player = exoPlayer
             }
-        },
-        update = {}
+        }
     )
 }
