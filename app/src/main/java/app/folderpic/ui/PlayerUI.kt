d67c55bbe47748be1e603538c2850fdd10550f3f
89449 <riko91209@gmail.com>
2025-09-14 15:03:52 +0700

    Create PlayerUI.kt

---
+++ app/src/main/java/app/folderpic/ui/PlayerUI.kt
@@ -0,0 +1,97 @@
+package app.folderpic.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Replay10
+import androidx.compose.material.icons.filled.Forward10
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlayerUI(
+    modifier: Modifier = Modifier,
+    isPlaying: Boolean,
+    onTogglePlay: () -> Unit
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "00:00",
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Slider(
+            value = 0f,
+            onValueChange = {},
+            track = { sliderState ->
+                SliderDefaults.Track(
+                    sliderState = sliderState,
+                    modifier = Modifier.height(2.dp)
+                )
+            },
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .size(36.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            Icon(Icons.Filled.Replay10, contentDescription = null)
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(
+            onClick = onTogglePlay,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .background(if(isPlaying) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.primary)
+        ) {
+            Icon(
+                if (isPlaying) Icons.Filled.Pause else Icons.Filled.PlayArrow,
+                contentDescription = null,
+                tint = if(isPlaying) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onPrimary
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .size(36.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            Icon(Icons.Filled.Forward10, contentDescription = null)
+        }
+    }
+}
